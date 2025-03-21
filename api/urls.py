dab8725e9eb1fcd66f4f57ab5c491f18353f3dfa
rohan-yeole-data-axle <141135316+rohan-yeole-data-axle@users.noreply.github.com>
2025-03-21 12:02:30 +0530

    added html editor with basic functinality

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('html-editor/', views.HTMLEditorView.as_view(), name='html_editor'),
 ]
